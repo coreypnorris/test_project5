610d8a057ce36a9ace5dc9630f9fb32b287310cc
Corey Norris <coreypnorris@gmail.com>
2015-10-01 04:12:43 +0000

    remove comments and whitespace

--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
-puts Dir.pwd
 require '/home/nitrous/code/public_comics'
 run PublicComics
--- public_comics.rb
+++ public_comics.rb
@@ -1,7 +1,5 @@
 require 'sinatra/base'
 
-# set :bind, '0.0.0.0'
-
 class PublicComics < Sinatra::Base
   get "/" do
     "hi"
